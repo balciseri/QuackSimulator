98c02552455e809ac4ce70a7692784edec007d3e
Stefano Balciseri <balciseri@gmail.com>
2013-08-29 22:40:29 +0200

    simulator aggiornato

--- src/Simulator.java
+++ src/Simulator.java
@@ -4,11 +4,15 @@ public class Simulator {
 			
 			IQuackable q1 = new MallardDuck();
 			IQuackable q2 = new RedHeadDuck();
+			IQuackable q3 = new RubberDuck();
+			IQuackable q4 = new DuckCall();
 			
 			System.out.println("DUCK SIMULATOR");
 			
 			q1.Quack();
 			q2.Quack();
+			q3.Quack();
+			q4.Quack();
 			
 		}
 }
