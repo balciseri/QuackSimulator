ae3976d559ff65e8d52e0b99b1f0c40aba018b91
Stefano Balciseri <balciseri@gmail.com>
2013-08-29 22:14:59 +0200

    Aggiunta classe Simulator

---
+++ src/Simulator.java
@@ -0,0 +1,15 @@
+
+public class Simulator {
+
+		public static void main(String[] args) {
+			
+			IQuackable q1 = new MallardDuck();
+			IQuackable q2 = new RedHeadDuck();
+			
+			System.out.println("DUCK SIMULATOR");
+			
+			q1.Quack();
+			q2.Quack();
+			
+		}
+}
